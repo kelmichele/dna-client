340d940fa961477e1752bc28282feb0c979e8b48
Kelli Parris <kelli@hsdesignhouse.com>
2018-05-08 08:10:38 -0500

    root path to home, not login

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, controllers: { registrations: 'registrations' }
 
+  devise_scope :user do
+	  # root 'devise/sessions#new'
+    get 'login', to: 'devise/sessions#new'
+  end
 
   devise_scope :user do
-	  root 'devise/sessions#new'
-    get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
 
-	get 'home', to: 'static_pages#home'
+  root 'static_pages#home'
+	# get 'home', to: 'static_pages#home'
 	get 'testing', to: 'static_pages#testing'
 	get 'faq', to: 'static_pages#faq'
 
