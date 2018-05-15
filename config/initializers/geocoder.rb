b56cc4320907c56e5fa74cb7f2c2e9714b438525
Kelli Parris <kelli@hsdesignhouse.com>
2018-05-15 09:36:12 -0500

    trying map display on heroku

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  :api_key => ENV['dna_staff_prod'],   # API key for geocoding service
+  :api_key => ENV['DNA_STAFF_PROD'],   # API key for geocoding service
   # api_key: nil,               # API key for geocoding service
   cache: Redis.new,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
