18f3060e76859bb68c50fd8a7e7a04c6cd4207cc
Kelli Parris <kelli@hsdesignhouse.com>
2018-05-15 16:27:21 -0500

    removed redis from geo

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -9,7 +9,7 @@ Geocoder.configure(
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   :api_key => ENV['DNA_STAFF_PROD'],   # API key for geocoding service
   # api_key: nil,               # API key for geocoding service
-  cache: Redis.new,                 # cache object (must respond to #[], #[]=, and #del)
+  # cache: Redis.new,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
